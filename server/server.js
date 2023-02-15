3f1bcca09d32093d46abd29b699d8b44f16bbe61
Ameer <ak2260650@gmail.com>
2023-02-15 15:58:21 +0530

    fixed socekts cors

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://chekt.up.railway.app"],
   },
 });
 const dotenv = require("dotenv");
