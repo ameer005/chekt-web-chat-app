df37db5cf439d4f16858189787e86b344852c7c5
Ameer <ak2260650@gmail.com>
2023-02-15 15:48:56 +0530

    .

--- client/hooks/useSockets.js
+++ client/hooks/useSockets.js
@@ -1,8 +1,13 @@
-import React from "react";
 import { io } from "socket.io-client";
 
+let URL = "https://chekt-backend.up.railway.app";
+
+if (process.env.NODE_ENV !== "production") {
+  URL = "http://127.0.0.1:5000";
+}
+
 const useSockets = () => {
-  const socket = io("http://127.0.0.1:5000");
+  const socket = io(URL);
   return socket;
 };
 
